72835ff3bdddd853848cbe63ae23a24f3f525558
zbw790 <52985032+zbw790@users.noreply.github.com>
2025-07-25 00:49:36 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o0wmlvqc9xG1V2S2XvtZ3U3YyBto',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	ucuz9wU2RG5Px7ML2Phv_sn1G3F-t-16VOdJ07022Lw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '7-22',
       festivals: [
