3290012846d6e120ff9bb0f016cd90d704814f1d
zbw790 <52985032+zbw790@users.noreply.github.com>
2025-10-17 00:52:37 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -122,6 +122,40 @@ const USER_CONFIG = {
         { keyword: 'meet_day', date: '2025-10-31' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '宝宝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o0wmlvqc9xG1V2S2XvtZ3U3YyBto',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'r0sxMOsFZsY5yiPxh7fX91JuZbcUO_7dpZEGd9wNX-k',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '7-22',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        // {
+        //   type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        // },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝宝', year: '1999', date: '07-22',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2025', date: '05-02',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2025', date: '05-02',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2025-07-22' },
+        // 结婚纪念日
+        // { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'meet_day', date: '2025-10-31' },
+      ],
+    },
   ],
 
 
